2457d6b3678b5b8924cc6bd37553422ca672710c
stemca <stevenmatthew.arce@gmail.com>
2025-10-05 00:26:00 -0700

    fix linting

--- alembic/env.py
+++ alembic/env.py
@@ -10,7 +10,7 @@ from src.core.database import Base
 from src.core.config import database_settings
 
 # import all the models here directly
-from src.users.models import User
+from src.users.models import User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
