eac009ca1d4d436c5a890e912795c863ebe4b203
stemca <stevenmatthew.arce@gmail.com>
2025-11-07 14:59:20 -0800

    added migration to make subject to contact table

--- src/models/contact.py
+++ src/models/contact.py
@@ -47,6 +47,7 @@ class Contact(Base):
         ForeignKey("user.id", ondelete="CASCADE", onupdate="CASCADE"),
         nullable=True,
     )
+    subject: Mapped[str] = mapped_column(String, nullable=False)
 
     def __repr__(self) -> str:
         return f"Contact(id={self.id}, full_name={self.full_name}, email={self.email}, message={self.message}, status={self.status}, priority={self.priority}, submitted_at={self.submitted_at}, user_id={self.user_id})"
