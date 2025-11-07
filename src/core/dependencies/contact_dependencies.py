bca58dbbeb4f66d58f714e9dbf743d252367ef8f
stemca <stevenmatthew.arce@gmail.com>
2025-11-07 15:24:56 -0800

    added more endpoints for contact

--- src/core/dependencies/contact_dependencies.py
+++ src/core/dependencies/contact_dependencies.py
@@ -4,13 +4,18 @@ from fastapi import Depends
 
 from src.core.deps import T_Database
 from src.repositories.contact_repository import ContactRepository
+from src.repositories.user_repository import UserRepository
 from src.services.contact_service import ContactService
 
 
 class ContactDependencies:
     def __init__(self, db: T_Database):
         self.contact_repository = ContactRepository(db)
-        self.service = ContactService(contact_repository=self.contact_repository)
+        self.user_repository = UserRepository(db)
+        self.service = ContactService(
+            contact_repository=self.contact_repository,
+            user_repository=self.user_repository,
+        )
 
 
 def get_contact_deps(db: T_Database) -> ContactDependencies:
--- src/repositories/contact_repository.py
+++ src/repositories/contact_repository.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timezone
+from uuid import UUID
+
+from sqlalchemy import insert, select
 
 from src.core.deps import T_Database
 from src.models.contact import (
@@ -25,3 +28,13 @@ class ContactRepository:
         await self.db.commit()
         await self.db.refresh(contact)
         return contact
+
+    async def get_by_id(self, id: UUID) -> Contact | None:
+        statement = select(Contact).where(Contact.id == id)
+        result = await self.db.execute(statement)
+        return result.scalar_one_or_none()
+
+    async def get_contacts(self, skip: int, limit: int) -> list[Contact]:
+        statement = select(Contact).limit(limit).offset(skip)
+        result = await self.db.scalars(statement)
+        return list(result.all())
